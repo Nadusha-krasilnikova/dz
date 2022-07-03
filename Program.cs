0b032e453ed2e0cdf9c2babbf9168930c8953bcb
Nadusha <nadusha230275@gmail.com>
2022-07-03 19:35:59 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// выяснить является ли число четным
+Console.WriteLine("введите число ");
+int a=int.Parse(Console.ReadLine());
+int b=2;
+if(a % b==1)
+{
+Console.WriteLine("является нечетным числом ");
+Console.Write(a);
+}
+else
+{
+Console.WriteLine("является четным числом ");
+Console.Write(a);
+}
